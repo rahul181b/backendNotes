c4883495aa8c58854dbdd72b3c37fecbfb9f4167
Rahul <bhaleraor17@gmail.com>
2024-07-25 02:01:53 +0530

    controller added

---
+++ session-2/controllers/currencies.controllers.js
@@ -0,0 +1,26 @@
+
+
+export const getCurrencies = (req, res) => {
+    console.log(req.query);
+    const { min_value } = req.query;
+    if (min_value) {
+        const result = data.filter((item) => Number(item.min_size) === Number(min_value))
+        //https://www.w3schools.com/jsref/jsref_number.asp
+        res.json(result)
+    }
+    else {
+        res.json(data);
+    }
+
+    // res.end();
+}
+
+module.exports = getCurrencies;
+
+
+/*
+    Module Scope -> Each file in JS can be treated as a self contained module which can 
+import elements from other modules and export elements out of it.
+    Any element defined in a file is not available in the other modules by 
+default unless it is exported and imported correctly.
+*/
--- session-2/index.js
+++ session-2/index.js
@@ -1,7 +1,7 @@
 // using express 
 //
 // 1.Create an app instance
-
+import { getCurrencies } from "./controllers/currencies.controllers";
 const currencies = require("./currencies.json");
 const data = currencies.data;
 
@@ -12,18 +12,33 @@ const app = express();
 const PORT = 8082;
 
 // 3.Adding a route
-app.get("/currencies", (req, res) => {
-    console.log("current route /")
-    // 4.We can work normally with res object like we did with HTTP file. 
-    // res.write(`<h1>hello<h1>`);
+// app.get("/currencies", (req, res) => {
+//     console.log("current route /")
+//     // 4.We can work normally with res object like we did with HTTP file. 
+//     // res.write(`<h1>hello<h1>`);
 
-    // 5. or with express installed we can write 
-    // res.send(`<h1>hello<h1>`); // for sending data back
-    // or 
-    // res.json({ serverStatus: "active" }); //to send JSON 
-    res.json(data);
-    res.end();
-})
+//     // 5. or with express installed we can write 
+//     // res.send(`<h1>hello<h1>`); // for sending data back
+//     // or 
+//     // res.json({ serverStatus: "active" }); //to send JSON 
+//     res.json(data);
+//     res.end();
+// })
+app.get("/currencies/:symbol", (req, res) => {
+    //the url should be http://localhost:8082/currencies/usd
+    const symbol = req.params.symbol;
+
+    console.log(symbol);
+    const result = data.find((item) => item.id.toLowerCase() === symbol.toLowerCase());
+    if (result) {
+        res.status(200).json(result);
+    } else {
+        res.sendStatus(404);
+    }
+
+});
+//using query parameters
+app.get("/currencies", getCurrencies);
 
 app.get("/", (req, res) => {
     res.send(`<h1>currencies data<h1>`);
