7d0814664b45f8a39ae1f90dc9e44eb5599c4e0c
Rahul <bhaleraor17@gmail.com>
2024-07-26 14:16:05 +0530

    added validation for age and gender

--- Session-3/controllers/users.controllers.js
+++ Session-3/controllers/users.controllers.js
@@ -17,23 +17,37 @@ const searchUsersByQuery = (req, res) => {
     console.log("query fun called")
     const { gender } = req.query;
     const { age } = req.query;
+
     if (gender && age) {
         const results = data.filter(
             (item) => item.gender === gender && Number(item.dob.age) >= Number(age)
         );
         res.json(results)
     }
+    // use joi for validations 
     else if (gender) {
+        if (!["male", "female"].includes(gender)) {
+            return res.status(422).json({ "message": "Gender to search can either be 'male' or 'female'" });
+        }
         const result = data.filter((item) => item.gender === gender)
         res.json(result);
     }
     else if (age) {
+        if (!Number(age)) {
+            return res.status(422).json({ message: "Age parameter should be a number" });
+        }
+        if (age >= 100 || age <= 0) {
+            return res.status(422).json({ "message": "Age out of bounds. It should be a number between 0 and 100" })
+        }
         const result = data.filter((item) => Number(item.dob.age) === Number(age));
         res.json(result);
     }
-    else {
-        res.sendStatus(404);
+    else if (!age && !gender) {
+        return res.status(422).json({
+            message: "Missing Search Parameters, search using age and/or gender",
+        });
     }
+
 }
 module.exports = {
     getAllUsers,
